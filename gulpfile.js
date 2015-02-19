8f8a972c6a243b8214a25f4016f1b45c029c552b
Lucas <lucas@agtlucas.com>
2015-02-19 14:40:39 -0200

    Update gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var transform = require('vinyl-transform');
+var uglify = require('gulp-uglify');
+
+gulp.task('browserify', function () {
+  var browserified = transform(function (filename) {
+    var b = browserify(filename);
+    return b.bundle();
+  });
+
+  return gulp.src('app.js')
+    .pipe(browserified)
+    .pipe(uglify())
+    .pipe(gulp.dest('app-min.js'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('app.js', ['default']);
+});
+
+gulp.task('default', ['browserify']);
